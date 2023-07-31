20c42396e29c6bc56b2603dc68d10b2d105b2e4c
Kess220 <prodvictorkaio@gmail.com>
2023-07-30 21:50:42 -0300

    sll

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,5 @@
 import pg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const { Pool } = pg;
@@ -8,5 +7,7 @@ const { Pool } = pg;
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
+
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
